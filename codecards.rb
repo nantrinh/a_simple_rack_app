ab39afa93f7eec41d50de1374dd462befea26084
Nancy Trinh <nancytrinh20@gmail.com>
2019-07-13 12:31:05 -0700

    Add code to read in an entire file and preprocess into array of cards

--- codecards.rb
+++ codecards.rb
@@ -1,6 +1,8 @@
 require_relative 'cards'
 require_relative 'gizzard'
 
+require 'pry'
+
 class CodeCards < Gizzard
   def call(env)
     case env['REQUEST_PATH']
@@ -14,6 +16,14 @@ class CodeCards < Gizzard
       headers = { 'Content-Type' => 'text/html' }
       binding_object = binding
       body = erb_result(:random_card, binding_object)
+    when '/0/0'
+      status = 200
+      headers = { 'Content-Type' => 'text/html' }
+      # todo: replace with something that reads in entire flashcards file 
+      cards = Cards.new.cards
+      # todo: render an html page that displays each term and definition
+      binding_object = binding
+      erb_result(:cards, binding_object)
     else
       status = 404
       headers = { 'Content-Type' => 'text/html', 'Content-Length' => '48' }
@@ -22,3 +32,14 @@ class CodeCards < Gizzard
     response(status, headers, body)
   end
 end
+
+data = File.read('data/1.txt')
+cards = data.split("\n\n# ").map{|card| card.split("\n---\n")}
+cards.each do |card|
+  card.each do |term, _|
+    term.sub!(/^# /, '')
+  end
+end
+
+binding.pry
+puts ''
