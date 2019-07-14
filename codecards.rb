69cf0be224b2df768b14f350774783fb1ee3330d
Nancy Trinh <nancytrinh20@gmail.com>
2019-07-14 12:17:17 -0700

    [ADD]: names of all sets on homepage and set pages

--- codecards.rb
+++ codecards.rb
@@ -10,20 +10,20 @@ def titles(index)
 end
 
 get '/' do
-  status = 200
-  headers = { 'Content-Type' => 'text/html' }
-  body = '<html><body><h1>Hello World</h1></body></html>'
+  @title = 'Sets' 
+  @set_names = File.readlines('data/set_names.txt')
+  erb :home
 end
 
 get '/random_card' do
   @term, @definition = Cards.new.random_card
-  status = 200
-  headers = { 'Content-Type' => 'text/html' }
   erb :random_card
 end
 
 get '/:user_id/:set_id' do |user_id, set_id|
   redirect not_found unless user_id.to_i.zero? && [0, 1, 2].include?(set_id.to_i)
+  @nav_title = 'Sets'
+  @set_names = File.readlines('data/set_names.txt')
   @cards = Cards.from_file("data/#{set_id}.txt")
   @title = titles(set_id.to_i) 
   erb :set
