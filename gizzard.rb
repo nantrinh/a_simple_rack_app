fe284e972504a735fc070ab68eee32297ab3b71b
Nancy Trinh <nancytrinh20@gmail.com>
2019-07-13 17:10:21 -0700

    Works only for evaluation and insertion

--- gizzard.rb
+++ gizzard.rb
@@ -4,14 +4,16 @@ class Gizzard
   end
 
   def erb_result(name, binding_object)
-    html = File.read("views/#{name}.html") 
+    html = File.read("views/#{name}.rhtml") 
     template_regex = /<%[ =].+? %>/
     matches = html.scan(template_regex)
+    p matches
+#    p matches.map {|x| eval(extract_code(x), binding_object)}
     matches.each do |match|
       if (match[2] == '=')
-        eval(extract_code(match), binding_object)
-      else
         html.sub!(match, eval(extract_code(match), binding_object))
+      else
+        eval(extract_code(match), binding_object)
       end
     end
     p html
