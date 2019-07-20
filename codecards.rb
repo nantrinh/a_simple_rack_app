4e3e42cd13d1af20144e0e67db1444b0f0849095
Nancy Trinh <nancytrinh20@gmail.com>
2019-07-20 11:40:33 -0700

    [ADD]: links to previous and next cards

--- codecards.rb
+++ codecards.rb
@@ -39,15 +39,16 @@ get '/:user_id/:set_id/flashcards/:card_id/:side' do
   @set_id = params['set_id'].to_i
   @user_id = params['user_id'].to_i
   @card_id = params['card_id'].to_i
+  @side = params['side']
 
   redirect not_found unless @user_id.zero? && \
     (0...@set_names.size).cover?(@set_id) && \
-    ['term', 'definition'].include?(params['side'])
+    ['term', 'definition'].include?(@side)
 
   @cards = Cards.from_file("data/#{@set_id}.txt")
   redirect not_found unless (0...@cards.size).cover?(@card_id)
 
-  if params['side'] == 'term'
+  if @side == 'term'
     @display = @cards[@card_id][0] 
     other_side = "definition"
   else
@@ -55,8 +56,17 @@ get '/:user_id/:set_id/flashcards/:card_id/:side' do
     other_side = "term"
   end
 
-  @flip_link = \
-    "/#{@user_id}/#{@set_id}/flashcards/#{@card_id}/#{other_side}" 
+  link_prefix = "/#{@user_id}/#{@set_id}/flashcards"
+
+  if @card_id > 0
+    @previous_card_link = "#{link_prefix}/#{@card_id - 1}/term" 
+  end
+
+  if @card_id < @cards.size - 2
+    @next_card_link = "#{link_prefix}/#{@card_id + 1}/term" 
+  end
+
+  @flip_link = "#{link_prefix}/#{@card_id}/#{other_side}" 
 
   @title = @set_names[@set_id] 
   erb :nav_sidebar do
