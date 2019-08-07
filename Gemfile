6b0f3a1fff9e8ac7f4ea7302592a34b4fab6c4d8
Nancy Trinh <nancytrinh20@gmail.com>
2019-08-07 14:45:50 -0700

    [TESTS]: post to create new set

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source "https://rubygems.org"
 
 ruby '2.5.3'
 gem 'sinatra', '~> 2.0.5'
+gem 'rack-test'
+gem 'minitest'
 
 group :production do
   gem "puma"
--- tests/codecards_test.rb
+++ tests/codecards_test.rb
@@ -17,24 +17,31 @@ class AppTest < Minitest::Test
   def test_index
     get '/'
     assert_equal 200, last_response.status
-    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
     assert_includes last_response.body, 'Create'
     assert_includes last_response.body, 'Sets'
   end
 
-  def test_new_set
+  def test_new_set_view
     get '/sets/new'
     assert_equal 200, last_response.status
-    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
     assert_includes last_response.body, 'Create a new set'
     assert_includes last_response.body, 'form'
+  end
+
+  def test_new_set_submission
+    post '/sets', set_name: 'ABC123 Set', cards_text_box: "# sample text xyz\n---\nsample definition\n\n# another term lala\n---\nsome other definition"
+    assert_equal 302, last_response.status
+    get last_response['Location']
+    assert_includes last_response.body, 'ABC123 Set'
+    assert_includes last_response.body, 'sample text xyz'
+    assert_includes last_response.body, 'lala'
+    assert_includes last_response.body, 'table'
   end
 
   # def test_edit_set
   #   # only works if there is a set to edit
   #   get '/sets/temp_set/edit'
   #   assert_equal 200, last_response.status
-  #   assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
   #   assert_includes last_response.body, 'Save'
   #   assert_includes last_response.body, 'form'
   # end
@@ -44,7 +51,6 @@ class AppTest < Minitest::Test
     set_names.each_with_index do |name, index|
       get "/sets/public/#{index}"
       assert_equal 200, last_response.status
-      assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
       assert_includes last_response.body, name 
       assert_includes last_response.body, 'Study' 
       assert_includes last_response.body, 'table' 
